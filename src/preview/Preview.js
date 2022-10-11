2eef9b98daddf73655c350618f9655e3650270e7
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-10-11 16:27:57 +0700

    feat: loading components

--- src/preview/Preview.js
+++ src/preview/Preview.js
@@ -2,13 +2,14 @@ import React from "react";
 import PageContainer from "../components/utilities/container/Container";
 import InputPreview from "./InputPreview";
 import ButtonPreview from "./ButtonPreview";
+import LoadingPreview from "./LoadingPreview";
 
 const Preview = () => {
 	return (
 		<PageContainer className="flex flex-col gap-10">
 			<InputPreview />
-
 			<ButtonPreview />
+			<LoadingPreview />
 		</PageContainer>
 	);
 };
