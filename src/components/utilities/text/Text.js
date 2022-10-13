f92a6cb9bde7579e31b938cedb2f3bf8adb6a643
diablo <quangan186@gmail.com>
2022-10-13 15:45:55 +0700

    fix: text + title responsive

--- src/components/utilities/text/Text.js
+++ src/components/utilities/text/Text.js
@@ -3,7 +3,7 @@ import React from "react";
 const Text = ({ children, className }) => {
 	return (
 		<p
-			className={`text-base leading-[24px] font-medium text-secondary-20 ${className}`}
+			className={`text-base lg:text-xl xl:text-2xl 2xl:text-3xl leading-[24px] font-medium text-secondary-20 ${className}`}
 		>
 			{children}
 		</p>
