2d29164b1e85bc5622a7f8a5b5ca9cbb74a5c3ee
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-10-21 09:22:54 +0700

    feat(ui): blog component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 
 import Preview from "./preview/Preview";
+import Blog from "./components/blog/Blog";
+import Container from "./components/utilities/container/Container";
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Preview />} />
+			<Route
+				path="/blog"
+				element={
+					<Container>
+						<Blog
+							title="Three Life Lessons From a Dying Man"
+							likeCount={324}
+							readCount={555}
+							author="Aaron Nichols"
+							date="20/10/2022"
+						/>
+					</Container>
+				}
+			/>
 		</Routes>
 	);
-
 };
 
 export default App;
