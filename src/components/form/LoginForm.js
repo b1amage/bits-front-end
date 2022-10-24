50c8c28c54381db94cc5a221931538f7c9f82d7d
quangan186 <61352012+quangan186@users.noreply.github.com>
2022-10-24 15:54:46 +0700

    chore: refactor login form code + change file location

---
+++ src/components/form/LoginForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Input from "../utilities/form/Input";
+
+const LoginForm = () => {
+  return (
+    <form className="flex flex-col gap-5 my-10">
+      <Input fluid label="email" required name="email" type="text" />
+      <Input fluid label="password" required name="password" type="password" />
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ src/views/LoginPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "../components/utilities/button/Button";
+import Logo from "../components/utilities/image/Logo";
+import Container from "../components/utilities/container/Container";
+import Checkbox from "../components/utilities/form/Checkbox";
+import Text from "../components/utilities/text/Text";
+import NavLink from '../components/navigation/NavLink'
+import LoginForm from "../components/form/LoginForm";
+
+const LoginPage = () => {
+	return (
+		<Container className="flex flex-col justify-center ">
+			<Logo className="mx-auto" />
+			
+			<LoginForm />
+
+			<div className="flex items-center justify-between">
+				<Checkbox label="Remember me?" labelClassName={"!text-secondary-20"} />
+				<NavLink to={'/forgot-password'} children={'Forgot Password?'} className={"!text-primary-100 font-medium"} />
+			</div>
+
+			<Button primary fluid className="my-10">
+				Sign in
+			</Button>
+
+			<Text className="text-center my-15 flex justify-center gap-3 items-center">
+				Don’t have an account?{" "}
+				<NavLink to={'/register'} className="!text-primary-100 !p-0 text-base lg:text-xl xl:text-2xl 2xl:text-3xl leading-[24px] font-medium text-center my-15 flex justify-center gap-3 items-center">Sign up</NavLink>
+			</Text>
+		</Container>
+	);
+};
+
+export default LoginPage;
