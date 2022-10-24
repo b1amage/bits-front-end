a7f5e8696f5199c35ed26353e38526e718192ced
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-10-24 09:01:26 +0700

    fix:router

--- src/App.js
+++ src/App.js
@@ -12,11 +12,10 @@ import HomePage from "./views/HomePage";
 const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<Preview />} />
+			<Route path="/" element={<HomePage />} />
 			<Route path="/forgot-password" element={<ForgotPasswordPage />} />
 			<Route path="/profile" element={<ProfilePage />} />
 			<Route path="/profile/:type" element={<ProfilePage />} />
-			<Route path="/" element={<HomePage />} />
 			<Route path="/preview" element={<Preview />} />
 			<Route path="/register" element={<RegisterPage />} />
 			<Route path="/login" element={<Login />} />
