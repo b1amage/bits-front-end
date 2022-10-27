6eef805b07671b54090c1c0e5f29c52338eb9902
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-10-27 11:44:24 +0700

    feat(ui): blog detail page

---
+++ src/views/BlogDetailPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from "../components/utilities/container/Container";
+
+const BlogDetailPage = () => {
+	return <Container></Container>;
+};
+
+export default BlogDetailPage;
