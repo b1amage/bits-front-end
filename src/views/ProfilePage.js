ac986ca0a2968cd3faa5c24404e3e4b2e3d72dac
diablo <quangan186@gmail.com>
2022-12-02 18:24:04 +0700

    fix: profile ui

--- src/views/ProfilePage.js
+++ src/views/ProfilePage.js
@@ -3,7 +3,7 @@ import UserInfo from "components/profile/UserInfo";
 
 const ProfilePage = () => {
 	return (
-		<div className="inline-block w-full bg-teriary-gray">
+		<div className="inline-block w-full bg-teriary-gray-20">
 			<UserInfo />
 			<PostList />
 		</div>
