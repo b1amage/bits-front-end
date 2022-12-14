5dd8d4d8eb297fce7f9114967d7d73ba88fb06ab
diablo <quangan186@gmail.com>
2022-12-14 23:22:04 +0700

    chore: fetch data for edit blog page

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import {
+  EditorState,
+  convertToRaw,
+  convertFromHTML,
+  ContentState,
+} from "draft-js";
+import draftToHtml from "draftjs-to-html";
+const EditorForm = ({content, setConvertedContent}) => {
+  var overview = content
+  // convert HTML content to EditorState
+  const contentDataState = ContentState.createFromBlockArray(
+    convertFromHTML(overview)
+  );
+  const editorDataState = EditorState.createWithContent(contentDataState);
 
-const EditorForm = ({
-  editorState,
-  handleEditorChange,
-  uploadImageCallBack,
-}) => {
+  const [editorState, setEditorState] = useState(editorDataState); // set initial state with original HTML content
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  };
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = draftToHtml(
+      convertToRaw(editorState.getCurrentContent())
+    );
+    setConvertedContent(currentContentAsHTML);
+  };
+
+  const uploadImageCallBack = (file) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      // xhr.open("POST", "https://bits-gt8m.onrender.com/api/image/upload-image");
+      xhr.open("POST", "/api/image/upload-image");
+      const data = new FormData();
+      data.append("image", file);
+      xhr.send(data);
+      xhr.addEventListener("load", () => {
+        var data = JSON.parse(xhr.responseText);
+
+        if (!data.image) {
+          alert(data.msg);
+          reject();
+          return;
+        }
+
+        // Magic Happens Here
+        console.log(data);
+        var response = { data: { link: data.image.src } };
+
+        resolve(response);
+        // console.log(response);
+      });
+      xhr.addEventListener("error", () => {
+        const error = JSON.parse(xhr.responseText);
+        reject(error);
+      });
+    });
+  };
   // const uploadImageCallBack = (file) => {
   // 	return new Promise((resolve, reject) => {
   // 		const data = new FormData();
