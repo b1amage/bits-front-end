729a97193dadfaed44717f6f960f616da6db2dab
diablo <quangan186@gmail.com>
2022-12-26 20:08:20 +0700

    fix: dashboard + profile UI + add load more in profile

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,6 +1,6 @@
 const Loading = () => {
 	return (
-		<div className="mx-auto w-[100px] h-[100px] md:w-[150px] md:h-[150px] lg:w-[150px] lg:h-[150px] bg-primary-100 rounded-lg animate-flip transition-all"></div>
+		<div className="mx-auto my-8 w-[100px] h-[100px] md:w-[150px] md:h-[150px] lg:w-[150px] lg:h-[150px] bg-primary-100 rounded-lg animate-flip transition-all"></div>
 	);
 };
 
--- src/components/post/PostStats.js
+++ src/components/post/PostStats.js
@@ -1,52 +1,42 @@
 import PostStat from "components/statistic/PostStat";
-import eye from "assets/svg/eye.svg";
+// import eye from "assets/svg/eye.svg";
 // import comment from "assets/svg/comment.svg";
 import like from "assets/svg/like.svg";
 
-const PostStats = ({ views, commentCount, heartCount, time, postType, className }) => {
-	return (
-		<div
-			className={`${className} flex h-full gap-2 lg:gap-8 md:py-2 flex-wrap`}
-		>
-			{postType == null || postType === "post" ? (
-				<>
-					<PostStat
+const PostStats = ({
+  views,
+  commentCount,
+  heartCount,
+  time,
+  postType,
+  className,
+}) => {
+  return (
+    <div
+      className={`${className} flex h-full gap-2 lg:gap-8 md:py-2 flex-wrap`}
+    >
+      <>
+        {/* <PostStat
 						src={eye}
 						alt={"views"}
 						quantity={views}
 						postType={postType}
-					/>
-					{/* <PostStat
+					/> */}
+        {/* <PostStat
 						src={comment}
 						alt={"comments"}
 						quantity={commentCount}
 						postType={postType}
 					/> */}
-					<PostStat
-						src={like}
-						alt={"likes"}
-						quantity={heartCount}
-						postType={postType}
-					/>
-				</>
-			) : postType === "latest" ? (
-				<PostStat time={time} postType={postType} />
-			) : (
-				<>
-					<PostStat
-						quantity={views}
-						detail={"viewers"}
-						postType={postType}
-					/>
-					<PostStat
-						quantity={views}
-						detail={"reads"}
-						postType={postType}
-					/>
-				</>
-			)}
-		</div>
-	);
+        <PostStat
+          src={like}
+          alt={"likes"}
+          quantity={heartCount}
+          postType={postType}
+        />
+      </>
+    </div>
+  );
 };
 
 export default PostStats;
